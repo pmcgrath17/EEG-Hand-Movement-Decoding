61fa892213ad0f74f551f3b7c8c963ee46e0e5b2
Patrick <patarmcgrath17@gmail.com>
2020-09-15 20:56:55 -0700

    Current progress

---
+++ MATLAB/gathered_data/txt_extractor.m
@@ -0,0 +1,57 @@
+%% Load data and Constants
+filename = 'C:\Users\mcgrathp\Documents\Undergrad_Research\EMG-Decoder-master\EMG-Decoder-master\python\saved_data\test_2020-09-11_13-24-16.json';
+json = jsondecode(fileread(filename));
+
+output_folder = 'C:\Users\mcgrathp\PycharmProjects\EEG-Hand-Movement-Decoding\raw_data\';
+
+%% Loop through and calculate features at each timestep
+n_features = 16;
+N = length(json.raw_list);
+z = zeros(N, n_features);
+
+% flip buffer so that the oldest sample is at the top
+buffer = flip(json.raw_list{1}, 1);
+%z(1, :) = calc_feature(buffer);
+len = size(json.raw_list{1}, 1);
+small_buffer = buffer_for_export(buffer, size(buffer, 1));
+save_data(small_buffer, output_folder, 1);
+for i=2:N
+    % length of new raw data packet
+    len = size(json.raw_list{i}, 1);
+    
+    % make room for the new samples
+    buffer = circshift(buffer, -len);
+    
+    % insert the new samples
+    buffer(end-len+1:end, :) = json.raw_list{i};
+    
+    % calculate features (For now, implementation is in Python...)
+    %z(i, :) = calc_feature(buffer);
+    
+    small_buffer = buffer_for_export(buffer, size(buffer, 1));
+    save_data(small_buffer, output_folder, i);
+end
+
+save_labels(json.finger_data, output_folder);
+
+
+% Grab subset of EEG samples to export
+function small_buffer = buffer_for_export(full_buffer, len)
+    num_samples = len - 30;
+    small_buffer = full_buffer(num_samples:len,:);
+end
+
+% Save EEG data to .txt file (each index corresponds to row number of
+% finger labels)
+function save_data(buffer, output_folder, index)
+    output_file = sprintf('Data_EEG_%d', index - 1);
+    output_file_name = strcat(output_folder, output_file);
+    writematrix(buffer, output_file_name, 'Delimiter', 'tab');
+end
+
+% Save finger labels to .txt file
+function save_labels(finger_data, output_folder)
+    output_file = sprintf('Data_Finger_Labels');
+    output_file_name = strcat(output_folder, output_file);
+    writematrix(finger_data, output_file_name, 'Delimiter', 'tab');
+end
---
+++ models/continuous_output/MLP.py
@@ -0,0 +1,77 @@
+from models.feature_calculation import feature_algorithms as processing
+from models.feature_calculation import PCA_on_PSD as pca
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plotter
+import sklearn
+
+#########################
+# Constants
+path_to_data_file = "../../raw_data/"
+label_file = "Data_Finger_Labels.txt"
+data_file_template = "Data_EEG_"
+sample_frequency = 1000
+num_frequency_bins = 50
+max_frequency = 30.0
+num_channels = 16
+#########################
+
+if __name__ == '__main__':
+    print("Running MLP")
+
+    # Read in Data
+    features = []
+    labels = np.loadtxt(path_to_data_file + label_file)
+    for index in range(len(labels)):
+        feature_row = np.loadtxt(path_to_data_file + data_file_template + str(index) + ".txt")
+        features += [np.transpose(feature_row, (1, 0))]
+    features = np.array(features)
+
+
+    # Construct frequency bins
+    bin_width = max_frequency / num_frequency_bins
+    freq_bins = np.zeros((num_frequency_bins, 2))
+    for i in range(num_frequency_bins):
+        freq_bins[i, 0] = i * bin_width
+        freq_bins[i, 1] = (i + 1) * bin_width
+
+
+    # Apply PSD to data
+    processed_features = processing.average_PSD_algorithm(X=features, sample_freq=sample_frequency, bins=freq_bins)
+
+    # Apply PCA to PSD data
+    stat_matricies = pca.calc_covariance_matrices(processed_features)
+    eig_vectors = pca.calc_eig_vects(stat_matricies)
+    projected_features = pca.project_onto_pcs(processed_features, eig_vectors, num_frequency_bins)
+
+    print(projected_features)
+
+    # Shuffle the data
+    features, labels = sklearn.utils.shuffle(projected_features, labels)
+    print(features.shape)
+    print(freq_bins.shape)
+
+    # Train models (one for each finger)
+    models = []
+    histories = []
+    for i in range(5):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(num_channels, freq_bins.shape[0])),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(1, activation='linear')
+        ])
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.MeanSquaredError(),
+                      metrics=['accuracy']
+        )
+
+        history = model.fit(features, labels[:, i], epochs=100, verbose=1, validation_split=0.4)
+        histories += [history]
+
+    # Evaluate model
+    #######
+    # There is no test-training split as of yet
+    #######
+
+    # Plot results
