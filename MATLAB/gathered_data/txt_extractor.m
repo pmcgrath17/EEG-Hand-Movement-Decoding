a468058ee12a07d59084c7d8ff0f0b623079a033
Patrick <patarmcgrath17@gmail.com>
2020-10-27 00:29:13 -0700

    Data samples

--- MATLAB/gathered_data/txt_extractor.m
+++ MATLAB/gathered_data/txt_extractor.m
@@ -8,6 +8,7 @@ output_folder = 'C:\Users\mcgrathp\PycharmProjects\EEG-Hand-Movement-Decoding\ra
 n_features = 16;
 N = length(json.raw_list);
 z = zeros(N, n_features);
+sample_size = 1000;
 
 % flip buffer so that the oldest sample is at the top
 buffer = flip(json.raw_list{1}, 1);
@@ -29,15 +30,29 @@ for i=2:N
     %z(i, :) = calc_feature(buffer);
     
     small_buffer = buffer_for_export(buffer, size(buffer, 1));
+    small_buffer = average_dataset(small_buffer, sample_size);
     save_data(small_buffer, output_folder, i);
 end
 
 save_labels(json.finger_data, output_folder);
 
+function averaged_buffer = average_dataset(buffer, sample_size)
+    averaged_buffer = buffer;
+    for i=sample_size + 1:length(buffer)
+        averaged_buffer(i, :) = mean(buffer(i - sample_size:i, :));
+    end
+end
+
+% Apply common average reference
+% Take means of rows and subtract mean from the original matrix
+function modified_matrix = common_average_reference(matrix)
+    mean_rows = mean(matrix, 2);
+    modified_matrix = matrix - mean_rows;
+end
 
 % Grab subset of EEG samples to export
 function small_buffer = buffer_for_export(full_buffer, len)
-    num_samples = len - 30;
+    num_samples = len - 200;
     small_buffer = full_buffer(num_samples:len,:);
 end
 
@@ -46,13 +61,14 @@ end
 function save_data(buffer, output_folder, index)
     output_file = sprintf('Data_EEG_%d', index - 1);
     output_file_name = strcat(output_folder, output_file);
-    writematrix(buffer, output_file_name, 'Delimiter', 'tab');
+    writematrix(common_average_reference(buffer), output_file_name, 'Delimiter', 'tab');
 end
 
 % Save finger labels to .txt file
 function save_labels(finger_data, output_folder)
-    shifted_finger_data = finger_data + 1;
-    shifted_finger_data = shifted_finger_data / 2;
+    %shifted_finger_data = finger_data + 1;
+    %shifted_finger_data = shifted_finger_data / 2;
+    shifted_finger_data = finger_data;
     output_file = sprintf('Data_Finger_Labels');
     output_file_name = strcat(output_folder, output_file);
     writematrix(shifted_finger_data, output_file_name, 'Delimiter', 'tab');
